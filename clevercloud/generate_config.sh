8dcb57a3923c8cdb186652b8558fbc4a33dead68
Loic Tosser <loic@kalvad.com>
2020-03-14 12:33:37 +0400

    generate_config

---
+++ clevercloud/generate_config.sh
@@ -0,0 +1,14 @@
+cat <<EOF > dist/conf.json
+{
+	"api": "https://api-$TAIGA_DOMAIN/api/v1/",
+	"eventsUrl": "wss://api-$TAIGA_DOMAIN/events",
+	"debug": "$DEBUG",
+	"publicRegisterEnabled": $PUBLIC_REGISTER_ENABLED,
+	"feedbackEnabled": true,
+	"privacyPolicyUrl": null,
+	"termsOfServiceUrl": null,
+	"GDPRUrl": null,
+	"maxUploadFileSize": null,
+	"contribPlugins": []
+}
+EOF
